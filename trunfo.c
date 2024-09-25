3b543f8da2e3821817b01fcacb94f0cc8a0e2338
João Victor <jooovictorjesus@gmail.com>
2024-09-25 14:01:58 +0000

    final rj

--- trunfo.c
+++ trunfo.c
@@ -2,19 +2,21 @@
 
 int main(){
    printf("Desafio trunfo\n");
-    char Estado = RJ;
-    char codigo da carta[A1];
-    char nome da cidade [Rio de Janeiro];
-    double população = 16,60000 ;
-    float area km = 1.200;
-    float PIB = 359,6 ;
-    int pontos turisticos = 20;
+    
 
-
-
-
-
-
+    char nome[30] = "Rio de Janeiro";
+    float populacao = 16.60000 ;
+    float  km = 1.20;
+    float PIB = 359.6 ;
+    int pontos_turisticos = 20;
+    
+    printf("cidade: %s\n", nome);
+    printf("km: %f\n", km);
+    printf("pupulação: %f\n", populacao);
+    printf("PIB: %f\n", PIB);
+    printf("pontos_turisticos: %d\n", pontos_turisticos);
+    
+    return 0;
 
 
 }
