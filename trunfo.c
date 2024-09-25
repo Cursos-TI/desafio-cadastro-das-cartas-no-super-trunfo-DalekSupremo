d1dd7d8e35d835d42841c1013d3bfd80060db4bb
João Victor <jooovictorjesus@gmail.com>
2024-09-25 13:10:03 +0000

    Rio de Janeiro

--- trunfo.c
+++ trunfo.c
@@ -2,9 +2,13 @@
 
 int main(){
    printf("Desafio trunfo\n");
-    char nome[40];
-    char letra;
-    float população
+    char Estado = RJ;
+    char codigo da carta[A1];
+    char nome da cidade [Rio de Janeiro];
+    double população = 16,60000 ;
+    float area km = 1.200;
+    float PIB = 359,6 ;
+    int pontos turisticos = 20;
 
 
 
