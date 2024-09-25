a441dd0e7a273e0764af77289b10c207207f35ce
João Victor <jooovictorjesus@gmail.com>
2024-09-25 14:10:47 +0000

    realmente a parte final

--- trunfo.c
+++ trunfo.c
@@ -5,15 +5,15 @@ int main(){
     
 
     char nome[30] = "Rio de Janeiro";
-    float populacao = 16.60000 ;
-    float  km = 1.20;
-    float PIB = 359.6 ;
+    int populacao = 16.60000 ;
+    float  km = 1.2;
+    int PIB = 753.83  ;
     int pontos_turisticos = 20;
     
     printf("cidade: %s\n", nome);
-    printf("km: %f\n", km);
-    printf("pupulação: %f\n", populacao);
-    printf("PIB: %f\n", PIB);
+    printf("km: %f kilometros quadrados \n", km);
+    printf("pupulação: %d milhões\n", populacao);
+    printf("PIB: %d bilhões reais \n", PIB);
     printf("pontos_turisticos: %d\n", pontos_turisticos);
     
     return 0;
